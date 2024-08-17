045c4102673261037665805c417ef2cce2afd91b
amCtrld <petrembugua@proton.me>
2024-08-18 01:03:03 +0300

    Profile section styling

--- portfolio_a/components/Layout.js
+++ portfolio_a/components/Layout.js
@@ -1,5 +1,5 @@
-import Navigation from './Navigation';
-import styles from '../styles/Layout.module.css';
+import Navigation from "./Navigation";
+import styles from "../styles/Layout.module.css";
 
 export default function Layout({ children, activeSection, setActiveSection }) {
   return (
@@ -9,8 +9,17 @@ export default function Layout({ children, activeSection, setActiveSection }) {
         <main className={styles.content}>{children}</main>
       </div>
       <div className={styles.rightHalf}>
-        <Navigation activeSection={activeSection} setActiveSection={setActiveSection} />
-        <div className={styles.photo}></div>
+        <Navigation
+          activeSection={activeSection}
+          setActiveSection={setActiveSection}
+        />
+        <div className={styles.profile}>
+          <div className={styles.photo}></div>
+          <div className={styles.title}>
+            <h2 className={styles.name}>Peter Mbugua</h2>
+            <p className={styles.introduction}>JavaScript Developer</p>
+          </div>
+        </div>
       </div>
     </div>
   );
