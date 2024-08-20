07edc985004ad16fac9efb15b430d62ce7f90246
amCtrld <petrembugua@proton.me>
2024-08-20 21:32:35 +0300

    Added project cards in Works component

--- portfolio_a/components/Works.js
+++ portfolio_a/components/Works.js
@@ -1,31 +1,63 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import styles from "../styles/Works.module.css";
 
 export default function Works() {
-  const [message, setMessage] = useState("Coming soon");
-  const [displayedMessage, setDisplayedMessage] = useState("");
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  useEffect(() => {
-    if (currentIndex < message.length) {
-      const timeoutId = setTimeout(() => {
-        setDisplayedMessage(
-          (prevContent) => prevContent + message[currentIndex]
-        );
-        setCurrentIndex((prevIndex) => prevIndex + 1);
-      }, 150); // Adjust this value to control typing speed
+  const worksData = [
+    {
+      id: 1,
+      title: "Project 1",
+      description: "Description of project 1.",
+      image: "../Images/Bed1.jpeg",
+      link: "https://link-to-project1.com",
+    },
+    {
+      id: 2,
+      title: "Project 2",
+      description: "Description of project 2.",
+      image: "../Images/Bed2.jpeg",
+      link: "https://link-to-project2.com",
+    },
+    {
+      id: 3,
+      title: "Project 3",
+      description: "Description of project 3.",
+      image: "../Images/Bed3.jpeg",
+      link: "https://link-to-project3.com",
+    },
+  ];
 
-      return () => clearTimeout(timeoutId);
-    }
-  }, [currentIndex, message]);
+  const handleClick = (index) => {
+    setCurrentIndex(index);
+  };
 
   return (
     <div className={styles.works}>
       <h2>Works</h2>
-      <p className={styles.message}>
-        {displayedMessage}
-        <span className={styles.cursor}></span>
-      </p>
+      <div className={styles.cardsContainer}>
+        {worksData.map((work, index) => (
+          <div
+            key={work.id}
+            className={`${styles.card} ${
+              currentIndex === index ? styles.activeCard : ""
+            }`}
+            onClick={() => handleClick(index)}
+          >
+            <a href={work.link} target="_blank" rel="noopener noreferrer">
+              <div
+                className={styles.cardImage}
+                style={{ backgroundImage: `url(${work.image})` }}
+              >
+                <div className={styles.cardContent}>
+                  <h3>{work.title}</h3>
+                  <p>{work.description}</p>
+                </div>
+              </div>
+            </a>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
